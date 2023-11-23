3c73534e471cdd61b18a991a2439421fdc417e77
Prabhakar <prabhakaryadav100.97@gmail.com>
2023-11-23 00:05:36 -0500

    Assignment 4

--- Assignment/assignment3/PersonalPortfolio/portfolio/urls.py
+++ Assignment/assignment3/PersonalPortfolio/portfolio/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from .views import login_view, logout_view, project_form_view
+
 
 app_name = 'portfolio'
 
 urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('', views.home, name='home'),
+    # path('portfolio/', PortfolioView.as_view(), name='portfolio'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('about/', views.about, name='about'),
+    path('add_project/', project_form_view, name='add_project')
 ]
--- Assignment/assignment3/PersonalPortfolio/portfolio/views.py
+++ Assignment/assignment3/PersonalPortfolio/portfolio/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+from django.views import View
+from .forms import ProjectForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
+
+@login_required(login_url='portfolio:login')
 def home(request):
-
+    
     name = "Prabhakar Yadav"
     photo_url = "https://avatars3.githubusercontent.com/Prabhu13"
     email = "py98@njit.edu"
@@ -20,6 +25,7 @@ def home(request):
 
     return render(request, 'portfolio/home.html', context)
 
+@login_required(login_url='portfolio:login')
 def portfolio(request):
     # Retrieve the project details from the model
     projects = Project.objects.all()
@@ -30,6 +36,7 @@ def portfolio(request):
 
     return render(request, 'portfolio/portfolio.html', context)
 
+@login_required(login_url='portfolio:login')
 def about(request):
     # Define information about yourself
     about_text = "I am a web developer with a passion for coding."
@@ -39,3 +46,40 @@ def about(request):
     }
 
     return render(request, 'portfolio/about.html', context)
+
+
+@login_required(login_url='portfolio:login')
+def project_form_view(request):
+    template_name = 'portfolio/project_form.html'
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            # Set the user in the form's instance before saving
+            form.instance.user = request.user
+            form.save()
+            return redirect('portfolio:portfolio')  # Redirect to the portfolio view
+    else:
+        form = ProjectForm()
+
+    return render(request, template_name, {'form': form})
+    
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('portfolio:portfolio')
+            else:
+                form.add_error(None, 'Invalid login credentials')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'portfolio/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('portfolio:login')
